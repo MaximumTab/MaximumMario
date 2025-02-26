f46406686f15f942f8a218aabe8cb183e1317a4b
BDragonSoldier <ricky.oberholzer@gmail.com>
2025-02-26 20:13:30 +1100

    bricks script instantiation

---
+++ Assets/Scripts/BricksLogic.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BricksLogic : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void BrickType(string BrickName,string Location)
+    {
+        
+    }
+}
