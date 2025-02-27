ec4646ffbe914811d2cceceb729d31e767e1033a
Jay <Byrnejay2001@gmail.com>
2025-02-27 12:12:46 +1100

    Update SideScrolling.cs

--- Assets/Scripts/SideScrolling.cs
+++ Assets/Scripts/SideScrolling.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SideScrolling : MonoBehaviour
 {
     private Transform player;
+    private float leftScreenBound;
 
     private void Awake()
     {
@@ -11,8 +12,14 @@ public class SideScrolling : MonoBehaviour
 
     private void LateUpdate()
     {
+        leftScreenBound = Camera.main.ViewportToWorldPoint(Vector3.zero).x;
+
         Vector3 cameraPosition = transform.position;
         cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
         transform.position = cameraPosition;
+
+        Vector3 playerPosition = player.position;
+        playerPosition.x = Mathf.Max(playerPosition.x, leftScreenBound);
+        player.position = playerPosition;
     }
 }
