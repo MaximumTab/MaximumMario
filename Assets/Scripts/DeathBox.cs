e0a5e92ea6195f7101ac665078bc6836156e084a
Jay <Byrnejay2001@gmail.com>
2025-02-27 23:01:54 +1100

    Deathbox fix code

--- Assets/Scripts/DeathBox.cs
+++ Assets/Scripts/DeathBox.cs
@@ -4,19 +4,18 @@ using UnityEngine.SceneManagement;
 public class DeathBox : MonoBehaviour
 {
     public int Respawn;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
     }
 
-    void OnTggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player")){
             AudioManager.Instance.PlayConditionSound("Mario Dies");
