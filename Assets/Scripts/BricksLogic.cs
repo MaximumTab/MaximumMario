497d159de839823d75b8045275affe3f15e2cb01
BDragonSoldier <ricky.oberholzer@gmail.com>
2025-02-28 21:19:24 +1100

    Brick fixing still need items

--- Assets/Scripts/BricksLogic.cs
+++ Assets/Scripts/BricksLogic.cs
@@ -1,75 +1,66 @@
 using System;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 
 public class BricksLogic : MonoBehaviour
 {
-    private string Name="No Block Chosen";
-    private string Location= "No Location Chosen";
-    [SerializeField] private Sprite[] DisplayBricks;
+    private string Name = "No Block Chosen";
+    private string Location = "No Location Chosen";
 
-    [SerializeField] private bool Invisible=false;
+    [SerializeField] private bool Invisible = false;
+    [SerializeField] private bool Lucky = false;
+    [SerializeField] private bool Smash = false;
+    [SerializeField] private bool isRandom = true;
+    [SerializeField] private string location;
+    [SerializeField] private GameObject[] RandomSeed;
+    [SerializeField] private GameObject GaurenteeObject;
+    [SerializeField] private int Hp=1;
+    private int i = 0;
 
     [SerializeField] private Animator BrickAnim;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         BrickAnim = GetComponent<Animator>();
+        BrickAnim.SetBool("Lucky",Lucky);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    public string BrickSet(string name, string location)
+    private void SpewObject()
     {
-        int i = 0;
-        switch (location)
+        if (isRandom)
         {
-            case "Underground":
-                Location = location;
-                i = 15;
-                break;
-            case "Overworld":
-                Location = location;
-                i = 0;
-                break;
-            default:
-                Location = "No Location Chosen";
-                break;
+            Instantiate(RandomSeed[UnityEngine.Random.Range(0,RandomSeed.Length)]);
         }
-        switch (name)
+        else
         {
-            case "Lucky Block":
-                Name = name;
-                break;
-            case "Smash Block":
-                Name = name;
-                i += 10;
-                break;
-            case "Safe Block":
-                Name = name;
-                i += 5;
-                break;
-            default:
-                Name = "No Block Chosen";
-                break;
+            Instantiate(GaurenteeObject);
         }
-        Spritebrick(i);
-        return Name+", "+Location;
     }
 
-    void Spritebrick(int i)
-    {
-        gameObject.GetComponent<SpriteRenderer>().sprite = DisplayBricks[i];
-    }
 
-    void OnTriggerEnter()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (Hp > 0)
+        {
+            if(!Smash)
+                Hp -= 1;
+            BrickAnim.Play("Brick Bounce", 0);
+        }
+        if(Hp==0&&!Smash)
+        {
+            Lucky = false;
+            BrickAnim.SetBool("Lucky",Lucky);
+            BrickAnim.Play("Hp",1);
+        }
+        else if(Smash)
+        {
+            
+        }
     }
 }
 
