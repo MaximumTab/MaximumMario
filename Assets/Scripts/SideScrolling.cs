f98c0732e983cf936090b86785ccbd63bbabef0d
Jay <Byrnejay2001@gmail.com>
2025-02-28 22:16:28 +1100

    Pipe madness

--- Assets/Scripts/SideScrolling.cs
+++ Assets/Scripts/SideScrolling.cs
@@ -5,6 +5,10 @@ public class SideScrolling : MonoBehaviour
     private Transform player;
     private float leftScreenBound;
 
+    public float height = 6.5f;
+     public float undergroundHeight = -9.5f;
+    public float undergroundThreshold = 0f;
+
     private void Awake()
     {
         player = GameObject.FindWithTag("Player").transform;
@@ -21,5 +25,15 @@ public class SideScrolling : MonoBehaviour
         Vector3 playerPosition = player.position;
         playerPosition.x = Mathf.Max(playerPosition.x, leftScreenBound);
         player.position = playerPosition;
+
+        
     }
+      public void SetUnderground(bool underground)
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.y = underground ? undergroundHeight : height;
+        transform.position = cameraPosition;
+    }
+
 }
+
